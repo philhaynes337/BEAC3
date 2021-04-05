394520ef7ed5e73b452b4670c59368defee3a8b7
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2021-04-05 14:02:03 -0500

    Update
    Update

--- src/config.js
+++ src/config.js
@@ -3,6 +3,20 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'testtoken',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/jwtauthdb',
+    // DATABASE: {
+    //     type: "postgres",
+    //     host: process.env.HOST,
+    //     port: 5432,
+    //     username: process.env.USERNAME,
+    //     password: process.env.PASSWORD,
+    //     database: process.env.DATABASE,
+    //     ssl: true,
+    //     extra: {
+    //         ssl: {
+    //             rejectUnauthorized: false,
+    //         }
+    //     }
+    // },
     JWT_SECRET: process.env.JWT_SECRET || 'test',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
 }
